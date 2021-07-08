3ac335e94771c8c4fbad5b9c7175a2050923eb1c
cca <cca@163.com>
2021-07-08 23:50:11 +0800

    add some status for search process

--- src/conponents/Search/index.jsx
+++ src/conponents/Search/index.jsx
@@ -6,13 +6,19 @@ export default class Search extends Component {
         //获取用户的输入(连续解构赋值+重命名)
 
         const {keyWordElement:{value:keyword}} = this
-        console.log(keyword) 
+        //发送请求前通知App更新状态
+        this.props.updateAppState({isFirst:false,isLoading:true})
+     
         //发送网络请求
         axios.get(`/api1/search/users?q=${keyword}`).then(
             response=>{
-                this.props.saveUsers(response.data.items)
+                //请求成功后通知App更新状态
+                this.props.updateAppState({isLoading:false,users:response.data.items})
             },
-            error=>{console.log('失败了',error);}
+            error=>{
+                //请求失败后通知App更新状态
+                this.props.updateAppState({isLoading:false,err:error.message})
+            }
         )
         
     }
