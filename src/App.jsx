58fe64dcb841bdd569fbffec2b0f64c313f1fb69
cca <cca@163.com>
2021-07-09 07:39:08 +0800

    git search with pusub

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,21 @@ import List from './conponents/List'
 
 
 export default class App extends Component {
-    state = {
-        users:[], //初始化状态，users初始值为数组
-        isFirst:true, //是否为第一次打开页面
-        isLoading:false, //标识是否处于加载中
-        err:'', //存储请求相关的错误信息
-    }
-    //更新App的state
-    updateAppState=(stateObj)=>{
-        this.setState(stateObj)
+
+    // //更新App的state
+    // updateAppState=(stateObj)=>{
+    //     this.setState(stateObj)
         
-    }
+    // }
+    //  updateAppState={this.updateAppState}
+    // {...this.state}
 
 
     render() {
         return (
             <div className="container">
-                <Search updateAppState={this.updateAppState}/>
-                <List {...this.state}/>
+                <Search/>
+                <List/>
             </div>
         )
     }
--- src/conponents/Search/index.jsx
+++ src/conponents/Search/index.jsx
@@ -1,23 +1,26 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-
+import PubSub, { clearAllSubscriptions } from 'pubsub-js'
 export default class Search extends Component {
     Search =()=>{
         //获取用户的输入(连续解构赋值+重命名)
 
         const {keyWordElement:{value:keyword}} = this
-        //发送请求前通知App更新状态
-        this.props.updateAppState({isFirst:false,isLoading:true})
+        //发送请求前通知List更新状态
+        // this.props.updateAppState({isFirst:false,isLoading:true})
+        PubSub.publish('atguigu',{isFirst:false,isLoading:true})
      
         //发送网络请求
         axios.get(`/api1/search/users?q=${keyword}`).then(
             response=>{
-                //请求成功后通知App更新状态
-                this.props.updateAppState({isLoading:false,users:response.data.items})
+                //请求成功后通知List更新状态
+                // this.props.updateAppState({isLoading:false,users:response.data.items})
+                PubSub.publish('atguigu',{isLoading:false,users:response.data.items})
             },
             error=>{
-                //请求失败后通知App更新状态
-                this.props.updateAppState({isLoading:false,err:error.message})
+                //请求失败后通知List更新状态
+                // this.props.updateAppState({isLoading:false,err:error.message})
+                PubSub.publish('atguigu',{isLoading:false,err:error.message})
             }
         )
         
